fc5c5d6edce6a57ff77ed8774dee83b652e80653
seeriand <andreas.eriksson@elekta.com>
2026-05-10 22:28:09 +0200

    Run preprocessor automatically

--- ccz80.py
+++ ccz80.py
@@ -4,24 +4,37 @@ from asmWriter import AsmWriter
 from astnodes import ASTContext
 from symbolTable import SymbolTable
 import argparse
-import pathlib
+import subprocess
+from pathlib import Path
 
 argParser = argparse.ArgumentParser()
 
 argParser.add_argument('filename')
 
+def preprocess(file : str | Path):
+    path = Path(file)
+    result = subprocess.run(["cpp", str(path)],
+                            text=True,
+                            stdout=subprocess.PIPE,
+                            stdin=subprocess.PIPE)
+    if result.returncode != 0:
+        raise RuntimeError(f"Error invocing cpp: {result.stderr})")
+
+    return result.stdout
+
 if __name__ == '__main__':
     args = argParser.parse_args()
-    inputPath = pathlib.Path(args.filename)
+    inputPath = Path(args.filename)
     outputPath = inputPath.with_suffix('.asm')
 
     asmFile = open(outputPath, "w")
     asmWriter= AsmWriter(asmFile)
     symbolTable = SymbolTable()
 
-    inputFile = open(inputPath, "r")
+    # inputFile = open(inputPath, "r")
 
-    ast = parser.parse(inputFile.read())
+    # ast = parser.parse(inputFile.read())
+    ast = parser.parse(preprocess(inputPath))
     astContext = ASTContext(symbolTable = symbolTable)
     blocks, dataSegment = astToThreeCode(ast, astContext)
     updateLive(blocks)
