43dbebf67f0d680a3f13eaee1327975c957da456
seeriand <andreas.eriksson@elekta.com>
2026-05-10 17:57:16 +0200

    Add ccz80 for invocing the compiler
    E.g. ccz80 test.c # Produces test.asm

---
+++ ccz80.py
@@ -0,0 +1,30 @@
+from parser import parser
+from compiler import astToThreeCode, updateLive, genCode, genDataSegment
+from asmWriter import AsmWriter
+from astnodes import ASTContext
+from symbolTable import SymbolTable
+import argparse
+import pathlib
+
+argParser = argparse.ArgumentParser()
+
+argParser.add_argument('filename')
+
+print(__name__)
+if __name__ == '__main__':
+    args = argParser.parse_args()
+    inputPath = pathlib.Path(args.filename)
+    outputPath = inputPath.with_suffix('.asm')
+
+    asmFile = open(outputPath, "w")
+    asmWriter= AsmWriter(asmFile)
+    symbolTable = SymbolTable()
+
+    inputFile = open(inputPath, "r")
+
+    ast = parser.parse(inputFile.read())
+    astContext = ASTContext(symbolTable = symbolTable)
+    blocks, dataSegment = astToThreeCode(ast, astContext)
+    updateLive(blocks)
+    genCode(blocks, asmWriter)
+    genDataSegment(dataSegment, asmWriter)
