b912fab5995e7ba59f46216d9472b545166ff464
seeriand <andreas.eriksson@elekta.com>
2026-05-14 18:21:20 +0200

    Move test_errorhandling to test_compileAST

--- test_compileAST.py
+++ test_compileAST.py
@@ -1,5 +1,5 @@
 import unittest
-from testutilities import compileBlockToIR
+from testutilities import compileBlockToIR, compile
 from symbolTable import SymbolTable
 from astnodes import *
 
@@ -71,6 +71,33 @@ class TestErrorHandling(unittest.TestCase):
             void foo(""")
         self.assertIn("Unexpected end of file", cts.exception.message)
         self.assertEqual(cts.exception.location.line, 3)
+
+    def test_missingFunction(self):
+        with self.assertRaises(CompileError) as ctx:
+            output = compile("""char main() {
+                                foo();
+                                return 0;
+                              }""")
+        self.assertEqual(ctx.exception.location.line, 2) 
+        self.assertEqual(ctx.exception.message, "Attempting to call unknown foo") 
+
+    def test_callingNonFunction(self):
+        with self.assertRaises(CompileError) as ctx:
+            output = compile("""void main() {
+                                    char foo;
+                                    foo();
+                              }""")
+        self.assertEqual(ctx.exception.location.line, 3) 
+        self.assertEqual(ctx.exception.message, "Attempting to call non-function foo") 
+
+    def test_callingWrongNumberOfArguments(self):
+        with self.assertRaises(CompileError) as ctx:
+            output = compile("""void foo(char a, char b);
+                                void main() {
+                                    foo(1);
+                              }""")
+        self.assertEqual(ctx.exception.location.line, 3) 
+        self.assertEqual(ctx.exception.message, "Attempting to call function foo with 1 arguments but expected 2") 
 
 
 
