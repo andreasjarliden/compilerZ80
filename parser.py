dd2ff9caceb2eeafe9977b738fc55fdb71a55859
seeriand <andreas.eriksson@elekta.com>
2026-07-13 23:01:35 +0200

    Fix line numbers for structures

--- parser.py
+++ parser.py
@@ -113,7 +113,7 @@ def p_unary_struct_field(p):
 
 def p_unary_struct_pointer_field(p):
     'unary : unary ARROW ID'
-    p[0] = StructFieldReference(Dereference(p[1]), p[3], location=loc(p, 3))
+    p[0] = StructFieldReference(Dereference(p[1], location=loc(p, 3)), p[3], location=loc(p, 3))
 
 def p_unary_primary(p):
     'unary : grouping'
