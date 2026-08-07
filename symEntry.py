528109cebfa9ec91425dd695af13d98049a9d9bf
seeriand <andreas.eriksson@elekta.com>
2026-08-07 20:26:10 +0200

    Use a single implemetation of isPointer

--- symEntry.py
+++ symEntry.py
@@ -9,6 +9,10 @@ class Operand(ABC):
     def type(self) -> Any:
         ...
 
+    @property
+    def isPointer(self):
+        return isinstance(self.completeType, PointerType)
+
 
 # Object semantics but with custom equalByValue function
 class SymEntry(Operand):
@@ -24,10 +28,6 @@ class SymEntry(Operand):
     @property
     def type(self):
         return simpleTypeForComplexType(self.completeType)
-
-    @property
-    def isPointer(self):
-        return isinstance(self.completeType, PointerType)
 
     def equalByValue(self, other):
         return self.name == other.name and self.completeType == other.completeType
@@ -53,14 +53,6 @@ class CastSymEntry(Operand):
     def type(self):
         return simpleTypeForComplexType(self.completeType)
 
-    @property
-    def isPointer(self):
-        # TODO create real pointer type
-        if isinstance(self.completeType, StructType):
-            return False
-        else:
-            return isinstance(self.completeType, PointerType)
-
     def equalByValue(self, other):
         return self.name == other.name and self.completeType == other.completeType
 
@@ -85,10 +77,6 @@ class Constant(Operand):
             return "int"
         else:
             return self.completeType
-
-    @property
-    def isPointer(self):
-        return isinstance(self.completeType, PointerType)
 
     def __repr__(self):
         return f"Constant {self.completeType} {self.value}"
